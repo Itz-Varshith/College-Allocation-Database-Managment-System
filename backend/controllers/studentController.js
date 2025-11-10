5e7a14ad90c68a62293c4df0ad6f18aed2dec8a5
S Varshith Reddy <svarshithreddy9121@gmail.com>
2025-11-10 15:53:44 +0530

    Added controllers for home page detail fetching

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -71,9 +71,128 @@ const addStudentController = async (req, res) => {
   }
 };
 
-// This controller will take ,ist of choices and add into the users list. 
-const addStudentPreferences= async (req,res)=>{
+// This controller will take ,ist of choices and add into the users list.
+const addStudentPreferences = async (req, res) => {};
 
-}
+const changeCurrentStatus = async (req, res) => {
+  try {
+    const data = req.body;
+    data.student_id = req.session.studentId;
+    if (!data.student_id || !data.statusChangeTo) {
+      return res.status(404).json({
+        success: false,
+        message: "Incorrect data format provided",
+      });
+    }
+    const santizedData = data.statusChangeTo.toLowerCase();
+    await prisma.student.update({
+      where: {
+        student_id: parseInt(data.student_id, 10),
+      },
+      data: {
+        current_status: santizedData,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      message: "Status changed successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.json({
+      success: false,
+      message: "Server error while changing the current state for the user",
+      error: error,
+    });
+  }
+};
 
-export { addStudentController, addStudentPreferences };
+/**
+ *
+ * @param {Request data includes preferred college of the student or preferred 
+ *        department and also the category of the student, here keep in mind for the 
+ *        frontend that category is mandatory but college_id and department_id are 
+ *        optional to send, if the user doesn't fill them send them as 0 to the 
+ *        backend} req
+ * @param {Gives a response list of college data along with all the data of the college 
+ *         i.e department id and all the related stuff} res
+ * @returns
+ */
+
+const predictStudentResults = async (req, res) => {
+  try {
+    // This is the data object being recieved from the frontend
+    const data = req.body;
+    // data.student_id = req.session.studentId;
+    if (!data.student_id || !data) {
+      return res.status(400).json({
+        success:false,
+        message:"Incorrect request format",
+      })
+    }
+    // Get studnet data from the data only selecting what data is required i.e only ranks and categories.
+    const studentData=await prisma.student.findFirst({
+      where:{
+        student_id:parseInt(data.student_id,10)
+      },
+      select:{
+       isRegistered:true,
+       general_rank:true,
+       category_id:true,
+       category_rank:true 
+      }
+    })
+    // Generally student here is registered but i used it to avoid malacious attacks with random IDs that might break into the system
+    if( !studentData || !studentData.isRegistered){
+      return res.status(401).json({
+        success:false,
+        message:"Please register before using the prediction feature",
+      })
+    }
+    // Now for the prediction of the seats which the user might get based on the data given from the frontend, Here as i mentioned in the req format above i am only going to use non zero ids of collge and department.
+    // Also only one id must be provided at a time
+    const college_id=parseInt(data.college_id,10);
+    const department_id=parseInt(data.department_id,10);
+    // Getting the data ready to query cause if the user doesn;t have any sort or preference then we need to include all the college so i am making a new array named college_id_to_query to fill with data
+    let college_id_to_query=[];
+    if(college_id==0){
+      const resp=await prisma.college.findMany({
+        select:{
+          college_id:true
+        }
+      })
+      college_id_to_query=resp.map((a)=>a.college_id);
+    }else{
+      college_id_to_query.push(college_id);
+    }
+    // Similar to the above one i am writing a query to get the details related to new department
+    let department_id_to_query=[];
+    if(department_id==0){
+      const resp=await prisma.department.findMany({
+        select:{
+          department_id:true
+        }
+      })
+      department_id_to_query=resp.map((a)=>a.department_id);
+    }else{
+      department_id_to_query.push(department_id);
+    }
+    // Now we have the sanitized data that we can use to query the details from the closing and opening rank table
+    
+    return res.status(200).json({
+      success:true,
+      message:"Data sanitization successfull",
+      department_id_to_query,
+      college_id_to_query
+    })
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server error while predicting student choices",
+      error: error,
+    });
+  }
+};
+
+export { addStudentController, addStudentPreferences, changeCurrentStatus, predictStudentResults };
