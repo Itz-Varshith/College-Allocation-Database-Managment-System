6173e860db636b420ecc49470e776f2a6e696b32
kushal-gunala <kushalgunala@gmail.com>
2025-11-16 01:01:23 +0530

    updated preferences and home

--- backend/routes/preferenceRouter.js
+++ backend/routes/preferenceRouter.js
@@ -2,7 +2,7 @@ import {Router} from "express"
 import {addAllStudentPreferences, addPreferences,getPreferences} from "../controllers/preferencesController.js"
 
 const prefernceRouter=new Router();
-prefernceRouter.get('/add',addPreferences)
+prefernceRouter.post('/add',addPreferences)
 prefernceRouter.get('/self/:id',getPreferences)
 prefernceRouter.get('/addAll',addAllStudentPreferences)
 export default prefernceRouter;
--- backend/routes/studentRouter.js
+++ backend/routes/studentRouter.js
@@ -1,10 +1,10 @@
 import {Router} from "express"
-import {addStudentController, addStudentPreferences,changeCurrentStatus,getStudentAllocation} from "../controllers/studentController.js"
+import {addStudentController, addStudentPreferences,changeCurrentStatus,getCurrentAllocationStatus,getStudentAllocation} from "../controllers/studentController.js"
 const studentRouter=new Router();
 
 studentRouter.get('/add',addStudentController)
 studentRouter.post('/preferences',addStudentPreferences)
 studentRouter.post('/getAllocationDetails',getStudentAllocation)
 studentRouter.post('/changeStatus',changeCurrentStatus)
-
+studentRouter.get('/getCurrentAllocation',getCurrentAllocationStatus)
 export default studentRouter;
