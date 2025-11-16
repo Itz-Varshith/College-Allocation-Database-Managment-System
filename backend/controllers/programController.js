900553ebf0be04ca3c8055f2e67bfaaf98ec7e6f
S Varshith Reddy <svarshithreddy9121@gmail.com>
2025-11-17 03:02:13 +0530

    Minor changes

--- backend/controllers/programController.js
+++ backend/controllers/programController.js
@@ -14,7 +14,7 @@ const addProgramController= async (req,res)=>{
                     }
                 })
             }
-        }
+        };
         return res.status(200).json({
             success:true,
             message:"Programs inserted successfully"
