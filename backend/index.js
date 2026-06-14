d9da03039349b3f93a9271d914c946f483c6fdc9
S Varshith Reddy <svarshithreddy9121@gmail.com>
2026-06-14 22:52:22 +0530

    FIx: CORS issue on backend fix

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:3000", 
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
